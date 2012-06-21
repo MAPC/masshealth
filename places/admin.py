667294dcc3b8ab34618ad674c2b6ac8efeec0620
Bill Freeman (ke1g) <bfreeman@appropriatesolutions.com>
2012-06-21 15:16:13 -0400

    Make it possible to run dev server on my desktop.
    While I'm accessing a suitable database remotely, I don't have enough
    stuff installed locally to have OSMGeoAdmin (no GDAL installed, for
    example).

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,9 @@
 from django.contrib.gis import admin
 from models import Place
 
-admin.site.register(Place, admin.OSMGeoAdmin)
+try:
+    _model_admin = admin.OSMGeoAdmin
+except AttributeError:
+    _model_admin = admin.ModelAdmin
+
+admin.site.register(Place, _model_admin)
