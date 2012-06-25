1fd22f0c4dba811b3d4a991b8d8c97822448b084
Christian Spanring <cspanring@gmail.com>
2012-06-25 15:17:19 -0400

    adds custom osmgeoadmin

--- programs/admin.py
+++ programs/admin.py
@@ -1,11 +1,16 @@
 from django.contrib.gis import admin
 from models import Program
 
+# default GeoAdmin overloads
+admin.GeoModelAdmin.default_lon = -7912100
+admin.GeoModelAdmin.default_lat = 5210000
+admin.GeoModelAdmin.default_zoom = 9
+
 def place_name(obj):
     return obj.place.name
 place_name.short_description = 'Town'
 
-class ProgramAdmin(admin.ModelAdmin):
+class ProgramAdmin(admin.OSMGeoAdmin):
     list_display = ['title', place_name]
 
 admin.site.register(Program, ProgramAdmin)
