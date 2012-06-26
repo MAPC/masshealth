c03912cd86a00ab8d32fd9e016f313891c37eaca
Bill Freeman (ke1g) <bfreeman@appropriatesolutions.com>
2012-06-26 19:54:34 -0400

    Part way toward visualization pre-load image generation.
    Have something broken in the JavaScript, prevents on load from
    completing, and a lot to do on the ajax view.

--- visualizations/urls.py
+++ visualizations/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns(
     'visualizations.views',
 
     url(r'^xml/(?P<vis_id>\d+)/(?P<place_id>\d+)/$', 'visualization_xml'),
+    url(r'^ajax/update/thumbnails/$', 'ajax_thumbnails_post'),
 )
