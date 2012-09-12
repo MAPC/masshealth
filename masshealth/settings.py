ddbc239bb3689b8458d47a7a4acb3324eb4284a6
Christian Spanring <cspanring@gmail.com>
2012-09-12 12:57:41 -0400

    path-tweaks in settings.py

--- masshealth/settings.py
+++ masshealth/settings.py
@@ -2,6 +2,7 @@
 from django.conf import global_settings as DEFAULT_SETTINGS
 import os
 import geonode
+
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT, PROJECT_NAME = os.path.split(PROJECT_DIR)
 GEONODE_ROOT = os.path.dirname(geonode.__file__)
@@ -73,7 +74,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = _rrel('htdocs', 'static', '')
+STATIC_ROOT = _rrel('htdocs', 'common_static', '')
 
 
 # URL prefix for static files.
@@ -86,7 +87,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     _rrel('common_static'),
-    os.path.join(GEONODE_ROOT, "static"),
+    os.path.join(GEONODE_ROOT, 'static')
 )
 
 # List of finder classes that know how to find static files in
@@ -132,9 +133,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    #_rrel('templates'),
-    '/home/mapc/masshealth/templates/',
-    '/home/mapc/geonode/geonode/templates',
+    _rrel('templates'), 
+    os.path.join(GEONODE_ROOT, 'templates'),
 )
 
 #
