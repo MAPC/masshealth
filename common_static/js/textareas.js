40a50c772b15b04f338bae2a1fc459d231ead04a
Marilyn Fletcher <mfletcher@appropriatesolutions.com>
2012-09-18 12:23:33 -0400

    updates to tiny_mce settings

--- common_static/js/textareas.js
+++ common_static/js/textareas.js
@@ -1,13 +1,12 @@
 tinyMCE.init({
 //	mode : "textareas",
-//        mode : "specific_textareas",
         mode: "exact",
         elements: "id_profile, id_program_desc, id_abstract, id_text, id_content, id_description",
 //        editor_selector : "mceEditor",
         theme : "advanced",
 	content_css : "/static/css/main.css",
-	plugins : "paste",
 //    plugins : "table,inlinepopups",
+	plugins : "paste",
 	paste_auto_cleanup_on_paste : true,
 	paste_remove_styles : true,
 	paste_remove_styles_if_webkit : true,
@@ -22,7 +21,7 @@ tinyMCE.init({
     // Theme options
 	theme_advanced_toolbar_location : "top",
 	theme_advanced_toolbar_align : "left",
-	theme_advanced_buttons1 : "fullscreen,styleselect,formatselect,separator,preview,separator,bold,italic,underline,strikethrough,separator,bullist,numlist,outdent,indent,separator,undo,redo,separator,link,unlink,anchor,separator,image,media,cleanup,help,separator,code",
+	theme_advanced_buttons1 : "fullscreen,styleselect,formatselect,separator,preview,separator,bold,italic,underline,strikethrough,separator,bullist,numlist,outdent,indent,separator,undo,redo,separator,link,unlink,anchor,separator,image,media,removeformat,cleanup,help,separator,code",
 	theme_advanced_buttons2 : "",
 	theme_advanced_buttons3 : "",
     height : "280",
@@ -33,9 +32,9 @@ tinyMCE.init({
 	extended_valid_elements : "a[name|href|target=_blank|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
 	fullscreen_settings : {
 		theme_advanced_path_location : "top",
-		theme_advanced_buttons1 : "fullscreen,separator,preview,separator,media,cut,copy,paste,separator,undo,redo,separator,search,replace,separator,code,separator,cleanup,separator,bold,italic,underline,strikethrough,separator,forecolor,backcolor,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,help",
+		theme_advanced_buttons1 : "fullscreen,separator,preview,separator,cut,copy,paste,separator,undo,redo,separator,search,replace,separator,code,separator,cleanup,separator,bold,italic,underline,strikethrough,separator,forecolor,backcolor,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,help",
 		theme_advanced_buttons2 : "removeformat,styleselect,formatselect,fontselect,fontsizeselect,separator,bullist,numlist,outdent,indent,separator,link,unlink,anchor",
-		theme_advanced_buttons3 : "sub,sup,separator,image,insertdate,inserttime,separator,tablecontrols,separator,hr,advhr,visualaid,separator,charmap,emotions,iespell,flash,separator,print"
+		theme_advanced_buttons3 : "sub,sup,separator,image,media,insertdate,inserttime,separator,tablecontrols,separator,hr,advhr,visualaid,separator,charmap,emotions,iespell,flash,separator,print"
 	},
     file_browser_callback : "CustomFileBrowser"
 });
